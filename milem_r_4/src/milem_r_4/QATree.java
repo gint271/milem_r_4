853d2673bc5c186117954968082e09cc236e7a57
Ryan Milem <gint271@gmail.com>
2014-05-13 11:38:07 -0700

    Started instantiator for the tree.

---
+++ milem_r_4/src/milem_r_4/QATree.java
@@ -0,0 +1,38 @@
+package milem_r_4;
+
+import java.io.*;
+
+/*
+ * Author: Ryan Milem
+ * 
+ * Programming Assignment #4
+ * 
+ * Date: 5-13-14
+ * 
+ * Description: Stores the decision tree for 20 Questions.  Also includes methods for dealing with modifying
+ * 		or traversing the tree.
+ */
+
+public class QATree 
+{
+	private node header, conductor;
+	
+	public QATree()
+	{
+		
+	}
+	
+	public QATree(File treeRecord)
+	{
+		try
+		{
+			BufferedReader recordReader = new BufferedReader(new FileReader(treeRecord));
+		}
+		catch(Exception e)
+		{
+			System.out.println("File failed to open.  Staring with an empty tree.");
+		}
+		
+		//todo: Reading the file and building the tree.
+	}
+}
