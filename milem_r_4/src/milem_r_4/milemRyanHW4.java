dc5ed36111005143265bfb2a841608dc01ebd608
gint271 <gint271@gmail.com>
2014-05-13 14:19:06 -0700

    Simple test code in the main program.

--- milem_r_4/src/milem_r_4/milemRyanHW4.java
+++ milem_r_4/src/milem_r_4/milemRyanHW4.java
@@ -1,10 +1,15 @@
 package milem_r_4;
 
+import java.io.File;
+
 public class milemRyanHW4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		File record = new File("F:\\record.txt");
+		
+		QATree myTree = new QATree(record);
+		myTree.print();
 	}
 
 }
