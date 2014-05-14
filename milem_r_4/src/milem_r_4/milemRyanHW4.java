94dc56a97629aeae7b9637802c5f1b929536bc78
r.milem <r.milem@SHL135-31.labs.clark.edu>
2014-05-14 09:50:44 -0700

    Started on adding to the tree.  Need to work on inserting and getting the new question.

--- milem_r_4/src/milem_r_4/milemRyanHW4.java
+++ milem_r_4/src/milem_r_4/milemRyanHW4.java
@@ -9,8 +9,9 @@ public class milemRyanHW4 {
 	{
 		File record = new File("F:\\record.txt");
 		Scanner keyboard = new Scanner(System.in);
-		
 		QATree myTree = new QATree(record);
+		String addedNoun;
+		String addedQuestion;
 		
 		while(myTree.hasNext())
 		{
@@ -25,6 +26,26 @@ public class milemRyanHW4 {
 			}
 		}
 		
+		myTree.printPrompt();
+	
+		if(keyboard.next().equals("y"))
+		{
+			System.out.println("I win!");
+		}
+		else
+		{
+			System.out.println("Darn");
+			System.out.println("What were you thinking of?");
+			addedNoun = keyboard.next();
+			
+			System.out.println("Enter a question for which yes would be the correct noun, and no would be the noun I guessed.");
+			addedQuestion = "";
+			while(keyboard.hasNext())
+			{
+				addedQuestion = addedQuestion + " " + keyboard.next();
+			}
+			System.out.println(addedQuestion);
+		}
 		
 	}
 
